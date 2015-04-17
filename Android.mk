d7693b3f89a67d3c7bb010295cdc2f0e561b41f2
Tom Marshall <tdm@cyngn.com>
2015-04-17 05:23:00 -0700

    recovery: Add lz4 libs
     * Required by transparent compresssion in make_ext4fs.
    
    Conflicts:
    
    	Android.mk
    	updater/Android.mk
    
    Change-Id: I566c9e1281cc0dd725e96db32da0a1c3c000030e

--- Android.mk
+++ Android.mk
@@ -76,7 +76,7 @@ LOCAL_STATIC_LIBRARIES := \
 ifeq ($(TARGET_USERIMAGES_USE_EXT4), true)
     LOCAL_CFLAGS += -DUSE_EXT4
     LOCAL_C_INCLUDES += system/extras/ext4_utils system/vold
-    LOCAL_STATIC_LIBRARIES += libext4_utils_static libz
+    LOCAL_STATIC_LIBRARIES += libext4_utils_static libz liblz4-static
 endif
 
 # This binary is in the recovery ramdisk, which is otherwise a copy of root.
